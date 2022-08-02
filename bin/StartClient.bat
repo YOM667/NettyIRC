fb3bcae97abcf6e4d7bfceed7493fe648b62c5f7
You_M <1055965862@qq.com>
2022-08-02 14:30:14 +0800

    输出的文件

---
+++ bin/StartClient.bat
@@ -0,0 +1,1 @@
+java -jar F:\User\Project\Java\KES-IRC-Server\out\artifacts\IRC_Client_jar\IRC-Client.jar
---
+++ bin/StartServer.bat
@@ -0,0 +1,1 @@
+java -jar F:\User\Project\Java\KES-IRC-Server\out\artifacts\IRC_Server_jar\IRC-Server.jar
